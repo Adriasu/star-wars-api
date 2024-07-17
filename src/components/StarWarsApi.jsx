425f2c668281f6144130cc583916f10970f4bc9b
Adriasu <adriana.suarez@students.gammatech.school>
2024-07-17 23:36:36 +0200

    peliculas luke

--- src/components/StarWarsApi.jsx
+++ src/components/StarWarsApi.jsx
@@ -26,6 +26,21 @@ const StarWarsApi = () => {
 
   infoDarthVader()
 
+  const moviesLukeSkywalker = async () => {
+    try {
+      const response = await fetch("https://swapi.dev/api/people/1/")
+      const dataLukeSkyWalker = await response.json()
+      const moviesData = dataLukeSkyWalker.films
+      console.log(moviesData);
+      
+    } catch (error) {
+      console.log(error);
+    }
+  
+  }
+
+  moviesLukeSkywalker()
+
   return <div></div>;
 };
 
