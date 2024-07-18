e984472c8a38c5bbb69aafd1115bd2c29a8850ee
Adriasu <adriana.suarez@students.gammatech.school>
2024-07-18 14:47:43 +0200

    terminado punto 5

--- src/components/StarWarsApi.jsx
+++ src/components/StarWarsApi.jsx
@@ -36,16 +36,30 @@ const StarWarsApi = () => {
       });
       const fetchLeiaPlanet = await fetch(leiaOrgana.homeworld);
       const dataPlanetLeia = await fetchLeiaPlanet.json()
-      console.log(dataPlanetLeia);
+      //console.log(dataPlanetLeia);
 
+      // 5. C-3PO
+      const C3PO = dataCharacters.find((character) => {
+        return character.name === "C-3PO"
+      })
+      const fetchInfoC3PO = await fetch(C3PO.url);
+      const dataC3PO = await fetchInfoC3PO.json();
+      //console.log(dataC3PO);
+      const linksFilmsC3PO = dataC3PO.films
 
-
-
-
-
-
-
-
+      const filmsDataC3PO = async () => {
+          const filmsC3PO = linksFilmsC3PO.map(async(film) => {
+            const fetchFilmsC3PO = await fetch(film)
+            const dataFilmsC3PO = await fetchFilmsC3PO.json()
+            return dataFilmsC3PO       
+          })
+          return await Promise.all(filmsC3PO)
+      }
+      const getFilmsC3PO = async () => {
+        const data = await filmsDataC3PO()
+        console.log(data);
+      }
+      getFilmsC3PO()
 
     } catch (error) {
       console.log(error);
