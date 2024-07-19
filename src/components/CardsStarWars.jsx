72618f0d9cc1fcf08b8e4a505577c2ebd7fc7e8e
Adriasu <adriana.suarez@students.gammatech.school>
2024-07-19 19:59:45 +0200

    terminado

--- src/components/CardsStarWars.jsx
+++ src/components/CardsStarWars.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useState, useEffect } from "react";
 
-const CardsStarWars = ({photo, name, homeworld, height, mass}) => {
+const CardsStarWars = ({ character, index }) => {
+  const [planets, setPlanets] = useState([]);
+
+  const fetchPlanest = async () => {
+    try {
+      const response = await fetch(character.homeworld);
+      const dataPlanets = await response.json();
+      const namePlanets = dataPlanets.name;
+      setPlanets(namePlanets);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchPlanest();
+  }, []);
+
   return (
-    <div className='border-[2px] border-black bg-slate-300 rounded-md w-[180px] h-[300px] p-3 flex flex-col justify-center items-center'>
-      <h1 className='border-[1px] border-black w-[150px] h-[200px] flex justify-center items-center'>({photo})</h1>
-      <h2>{name}</h2>
-      <h3 className='text-sm'>Lives in  </h3>
-      <div className='flex justify-between w-[150px] text-sm'>
-        <p>Height: {height}</p>
-        <p>Mass: {mass}</p>
+    <div className="border-[2px] border-black bg-slate-300 rounded-md w-[180px] h-[300px] p-3 flex flex-col justify-center items-center">
+      <h1 className="border-[1px] border-black w-[150px] h-[200px] flex justify-center items-center bg-slate-500 rounded-md">
+        ({index})
+      </h1>
+      <h2>{character.name}</h2>
+      <h3 className="text-sm">Lives in {planets}</h3>
+      <div className="flex justify-between w-[150px] text-sm">
+        <p>Height: {character.height}</p>
+        <p>Mass: {character.mass}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CardsStarWars
+export default CardsStarWars;
--- src/components/StarWarsUserList.jsx
+++ src/components/StarWarsUserList.jsx
@@ -9,34 +9,21 @@ const StarWarsUserList = () => {
       const response = await fetch("https://swapi.dev/api/people");
       const data = await response.json();
       const dataCharacters = data.results;
-      setCharacters(dataCharacters)
+      setCharacters(dataCharacters);
     } catch (error) {
       console.log(error);
     }
   };
 
   useEffect(() => {
-    fetchCharacters()
-  },[])
-
-  console.log(characters);
+    fetchCharacters();
+  }, []);
 
   return (
     <div className="m-auto grid grid-cols-3 gap-12">
-      {characters.map((character, index) => {
-        return(
-            <CardsStarWars
-            key={index}
-            photo={index}
-            name={character.name}
-            
-            height={character.height}
-            mass={character.mass}
-            />
-        )
-      })
-
-      }
+      {characters.map((character, i) => {
+        return <CardsStarWars key={i} character={character} index={i+1} />;
+      })}
     </div>
   );
 };
